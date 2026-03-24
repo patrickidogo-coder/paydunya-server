35b3b221778691f2f8f12fcf1ed64a40e473aec7
patrickidogo-coder <patrickidogo@icloud.com>
2026-03-24 11:00:53 +0000

    server.js

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req,res)=>{
+res.send("Serveur PayDunya actif");
+});
+
 app.post("/payer", (req, res) => {
 
 res.json({
 
 masterKey: "O3SWgWMR-32y7-6Kfg-7eFc-sXKbOrVCjHRi",
-privateKey: "test_private_n5li0sOB70LWXPuckEhWmV091qq",  
-publicKey: "test_public_LMrF05p8PN53QZX63SZFBgNDdJt",
-token: "yV4kJUrzU1uZe8DgdAdx",
 
-invoice: {
-total_amount: req.body.montant,
-description: "Commande nourriture"
-}
+privateKey: "test_private_n5li0sOB70LWXPuckEhWmV091qq",
+
+publicKey: "test_public_LMrF05p8PN53QZX63SZFBgNDdJt",
+
+token: "yV4kJUrzU1uZe8DgdAdx"
 
 });
 

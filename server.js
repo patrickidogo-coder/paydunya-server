3ce769369e50a9506e49633f49e855c04a5ae5a9
patrickidogo-coder <patrickidogo@icloud.com>
2026-03-24 13:21:27 +0000

    server.js

--- server.js
+++ server.js
@@ -7,23 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/payer", (req, res) => {
-res.send("Serveur PayDunya actif");
-});
-
-app.post("/payer", (req, res) => {
-
-res.json({
-
-masterKey: "O3SWgWMR-32y7-6Kfg-7eFc-sXKbOrVCjHRi",
-
-privateKey: "test_private_n5li0sOB70LWXPuckEhWmV091qq",
-
-publicKey: "test_public_LMrF05p8PN53QZX63SZFBgNDdJt",
-
-token: "yV4kJUrzU1uZe8DgdAdx"
-
-});
-
+   res.json({
+      masterKey: "O3SWgWMR-32y7-6Kfg-7eFc-sXKbOrVCjHRi",
+      privateKey: "test_private_n5li0sOB70LWXPuckEhWmV091qq",
+      publicKey: "test_public_LMrF05p8PN53QZX63SZFBgNDdJt",
+      token: "yV4kJUrzU1uZe8DgdAdx"
+   });
 });
 
 const PORT = process.env.PORT || 3000;
